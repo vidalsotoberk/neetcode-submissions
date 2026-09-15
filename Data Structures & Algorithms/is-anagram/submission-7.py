cd23d7564aed81449734f058769d452cabd25c18
Vidal Soto <vidalsoto0831@berkeley.edu>
2026-09-14 20:59:31 -0700

    Add: is-anagram - submission-7

---
+++ Data Structures & Algorithms/is-anagram/submission-7.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s_count = {}
+        t_count = {}
+        for c in s:
+            if c not in s_count:
+                s_count[c] = 1
+            else:
+                s_count[c] += 1
+        for c in t:
+            if c not in t_count:
+                t_count[c] = 1
+            else:
+                t_count[c] += 1
+        
+        return s_count == t_count

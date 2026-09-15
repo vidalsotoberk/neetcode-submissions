1621023ff60668b35b8f505649a3aa41916547aa
Vidal Soto <vidalsoto0831@berkeley.edu>
2026-09-15 08:34:00 -0700

    Add: anagram-groups - submission-6

---
+++ Data Structures & Algorithms/anagram-groups/submission-6.py
@@ -0,0 +1,12 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        hashmap = defaultdict(list)
+        for str in strs:
+            count = [0] * 26
+
+            for c in str:
+                count[ord(c) - ord('a')] += 1
+
+            hashmap[tuple(count)].append(str)
+
+        return list(hashmap.values())
